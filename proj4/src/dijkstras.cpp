2a05ad4b98e8cc0fca2dfc32141d4d4d7eb5c851
Grant Michael Alderson <galderso@hydra1.eecs.utk.edu>
2023-03-31 10:45:46 -0400

    finished dijkstras

--- proj4/src/dijkstras.cpp
+++ proj4/src/dijkstras.cpp
@@ -1,55 +1,95 @@
-// dijsktras.cpp
+//Grant Alderson and Sam Cradock
+//Citation: https://www.techiedelight.com/find-shortest-path-source-destination-matrix-satisfies-given-constraints/ and planks notes
+//This program finds the shortest path for a runner using dijkstras alorithm
 
-// Main Execution
-// dijsktras.cpp
 
+#include<vector>
+#include<algorithm>
 #include <iostream>
 #include <string>
 #include <map>
 #include <queue>
 
 using namespace std;
-// Main Execution
 
-struct Node {
-int total_weight;
-int x,y;
+
+struct Node{
+	int tile_weight;//weight of current node
+	pair<int,int> node1;//current node
+	pair<int,int> node2;//node before
+	bool const operator<( Node a)const {
+		return a.tile_weight<tile_weight;
+	}
 }; 
 
 
-int main(int argc, char *argv[]) {
-    map<char,int> weight;
-	int numtiles,tileweight;
-	char tile;
-	cin>>numtiles;
-	for(int i=0;i<numtiles;i++){
-		cin>>tile>>tileweight;
-		weight[tile]=tileweight;
-	}
-
+int main(int argc, char *argv[]){
+	int numtiles, tilenum;
 	int sizex,sizey;
-	char inputtile;
-	cin>>sizex>>sizey;
-	int board[sizex][sizey];
-	for(int i =0;i<sizex;i++){
-		for(int j =0;j<sizey;j++){
-		cin>>inputtile;
-		board[i][j]=weight.find(inputtile)->second;
+	int startx,starty;
+	int destx,desty;
+	string tile;
+	map<string, int> weight;
+	priority_queue<Node> cpath;
+	int dist=0;
+	map<pair<int,int>,pair<int,int>> edges;//edges between the nodes in the struct
+	cin >> numtiles;
+	for(int i=0; i < numtiles; i++){//takes chars and their weights
+		cin >>tile >> tilenum;
+		weight[tile] = tilenum;
+	}
+	cin >>sizex >>sizey;
+	string board[sizex][sizey];
+	for(int i=0; i < sizex; i++){//creates the board
+		for(int j=0; j<sizey; j++){
+			cin >> board[i][j];
+
 		}
 	}
-	int sourcex,sourcey,destx,desty;
-	cin>>sourcex>>sourcey;
-	cin>>destx>>desty;
+	Node v;
+	cin >>startx>> starty;
+	cin >>destx>> desty;
+	cpath.push({0, {destx,desty},{destx,desty}});
+
+	while(cpath.size()!=0){//loop until struct is empty
+		v = cpath.top();
+		cpath.pop();//pop top node
+		if((edges.count(v.node1))){//if edge is not already in map
+			continue;
+		}else{
+		edges[v.node1] = v.node2;//place edge into map
+		dist=v.tile_weight;//update weight
+		if(v.node1.first == startx && v.node1.second == starty){//checks if at starting node
+			break;  
+
+		}
+		}
+		
+		int x=v.node1.first;
+		int y=v.node1.second;
+		if((y+1) < sizey){//check up
+			cpath.push({v.tile_weight+weight[board[x][y+1]],{x, y+1},{x, y}});
+		}
+		if((y-1)>-1){//check down
+			cpath.push({v.tile_weight+weight[board[x][y-1]],{x, y-1},{x, y}});
+		} 
+		if((x+1) < sizex){//check right
+			cpath.push({v.tile_weight+weight[board[x+1][y]],{x+1, y},{x, y}}); 
+		}
+		if((x-1) >-1){//check left
+			cpath.push({v.tile_weight+weight[board[x-1][y]],{x-1, y},{x, y}});
+		}
 
 
-queue<Node> path;
+	}   
 
-path.push({0,destx,desty});
-while(!path){
-Node k=path.top();
-path.pop();
-
-
+	cout <<dist<<endl;//print
+	pair<int, int> temp = v.node1;
+	while(temp.first !=destx||temp.second !=desty){
+		cout << temp.first<< " " <<temp.second << endl;
+		temp = edges[temp];
+	}
+	cout << temp.first<<" " <<temp.second << endl;
 
 }
-}
+
